ba270a768cf3b9984cff4a7eccfb02f57b460c2c
eattato <80892781+eattato@users.noreply.github.com>
2022-09-22 16:41:24 +0900

    엄준식한 버그 고침

--- src/main/resources/static/category/js/script.js
+++ src/main/resources/static/category/js/script.js
@@ -1,39 +1,43 @@
-function httpGet(url)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+function httpGet(url) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
 
 const setViewMode = (viewMode) => {
-    if (viewMode == "simple" || viewMode == "exact") {
-        $.cookie("viewmode", viewMode, { expires: 2147483647 });
-        if (viewMode == "simple") {
-            $(".post_simple").css({display: "block"});
-            $(".post_exact").css({display: "none"});
-        } else if (changedValue == "exact") {
-            $(".post_simple").css({display: "none"});
-            $(".post_exact").css({display: "block"});
-        }
+  if (viewMode == "simple" || viewMode == "exact") {
+    $.cookie("viewmode", viewMode, { expires: 2147483647 });
+    if (viewMode == "simple") {
+      $(".post_simple").css({ display: "block" });
+      $(".post_exact").css({ display: "none" });
+    } else if (viewMode == "exact") {
+      $(".post_simple").css({ display: "none" });
+      $(".post_exact").css({ display: "block" });
     }
-}
+  }
+};
 
 $(() => {
-    // 보기 모드 쿠키 읽기
-    let viewMode = $.cookie("viewmode")
-    if (viewMode == undefined || (viewMode != "simple" && viewMode != "exact")) {
-        viewMode = "simple";
-    }
-    $("input[name='view_mode']").change(() => {
-        let changedValue = $("input[name='view_mode']:checked").val();
-        setViewMode(changedValue);
-    });
-    setViewMode(viewMode);
+  // 보기 모드 쿠키 읽기
+  let viewMode = $.cookie("viewmode");
+  if (viewMode == "exact") {
+    $("#view_simple").prop("checked", false);
+    $("#view_exact ").prop("checked", true);
+  }
 
-    // 카테고리 원본 가져오고 로딩 표시
-    var postSimple = $(".post_simple origin"); // 카테고리 버튼 원본
-    var postExact = $(".post_exact origin");
+  if (viewMode == undefined || (viewMode != "simple" && viewMode != "exact")) {
+    viewMode = "simple";
+  }
+  $("input[name='view_mode']").change(() => {
+    let changedValue = $("input[name='view_mode']:checked").val();
+    setViewMode(changedValue);
+  });
+  setViewMode(viewMode);
 
-    // httpRequest("GET", "http://localhost:8888/getcategory?page=")
+  // 카테고리 원본 가져오고 로딩 표시
+  var postSimple = $(".post_simple origin"); // 카테고리 버튼 원본
+  var postExact = $(".post_exact origin");
+
+  // httpRequest("GET", "http://localhost:8888/getcategory?page=")
 });
